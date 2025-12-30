08a07d5f1027e4fe6f73a7be26762f98705356b2
Arthur <arthurfelipe5567@gmail.com>
2025-12-30 12:33:37 -0300

    fix: change the repository interface used
    i was using Repository but should be using JpaRepository

--- src/main/java/com/voluntree/backend/repository/UserRepository.java
+++ src/main/java/com/voluntree/backend/repository/UserRepository.java
@@ -1,9 +1,9 @@
 package com.voluntree.backend.repository;
 
-import org.springframework.data.repository.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.voluntree.backend.domain.User;
 
-public interface UserRepository extends Repository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }

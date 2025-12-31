270e10a74135b99d592e38eff5fc52979ab89c6a
Arthur <arthurfelipe5567@gmail.com>
2025-12-30 21:25:37 -0300

    refactor: split the insert triggers migration

--- src/main/resources/db/migration/V3__Add_registration_triggers.sql
+++ src/main/resources/db/migration/V3__Add_registration_triggers.sql
@@ -1,19 +1,4 @@
 CREATE
-OR REPLACE FUNCTION vrf_activity_creator () RETURNS TRIGGER AS $$
-BEGIN
-  IF NOT EXISTS(SELECT 1 FROM users WHERE id = NEW.organization_id AND user_type = 'ORGANIZATION') THEN
-    RAISE EXCEPTION USING MESSAGE = 'Organização associada à atividade não encontrado!', ERRCODE = 23514;
-  END IF;
-
-  RETURN NEW;
-END;
-$$ language plpgsql;
-
-CREATE
-OR REPLACE TRIGGER vrf_activity_creator BEFORE INSERT ON activity FOR EACH ROW
-EXECUTE FUNCTION vrf_activity_creator ();
-
-CREATE
 OR REPLACE FUNCTION vrf_registration_creator_and_spots () RETURNS TRIGGER AS $$
 DECLARE
   total_spots SMALLINT;

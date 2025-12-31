603aecd0b2781186b3eae2dc0137597a1e066794
Arthur <arthurfelipe5567@gmail.com>
2025-12-30 21:04:37 -0300

    feat: add insert triggers migration

---
+++ src/main/resources/db/migration/V2__Add_insert_triggers.sql
@@ -0,0 +1,40 @@
+CREATE
+OR REPLACE FUNCTION vrf_activity_creator () RETURNS TRIGGER AS $$
+BEGIN
+  IF NOT EXISTS(SELECT 1 FROM users WHERE id = NEW.organization_id AND user_type = 'ORGANIZATION') THEN
+    RAISE EXCEPTION USING MESSAGE = 'Organização associada à atividade não encontrado!', ERRCODE = 23514;
+  END IF;
+
+  RETURN NEW;
+END;
+$$ language plpgsql;
+
+CREATE
+OR REPLACE TRIGGER vrf_activity_creator BEFORE INSERT ON activity FOR EACH ROW
+EXECUTE FUNCTION vrf_activity_creator ();
+
+CREATE
+OR REPLACE FUNCTION vrf_registration_creator_and_spots () RETURNS TRIGGER AS $$
+DECLARE
+  total_spots SMALLINT;
+  occupied_spots BIGINT;
+BEGIN
+  IF NOT EXISTS(SELECT 1 FROM users WHERE id = NEW.volunteer_id AND user_type = 'VOLUNTEER') THEN
+    RAISE EXCEPTION USING MESSAGE = 'Usuário não pode se inscrever na atividade, pois não é voluntário!', ERRCODE = 23514;
+  END IF;
+
+  SELECT spots INTO STRICT total_spots FROM activity where id = NEW.activity_id FOR UPDATE;
+
+  SELECT count(id) INTO occupied_spots FROM registration where activity_id = NEW.activity_id;
+
+  IF occupied_spots >= total_spots THEN
+    RAISE EXCEPTION USING MESSAGE = 'Usuário não pode se inscrever na atividade, vagas já preenchidas!', ERRCODE = 23514;
+  END IF;
+
+  RETURN NEW;
+END;
+$$ language plpgsql;
+
+CREATE
+OR REPLACE TRIGGER vrf_registration_creator_and_spots BEFORE INSERT ON registration FOR EACH ROW
+EXECUTE FUNCTION vrf_registration_creator_and_spots ();

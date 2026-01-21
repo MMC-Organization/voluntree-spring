cdb091a7e8f5f001fd6d5c4c8865a9d3a04d1028
Arthur <arthurfelipe5567@gmail.com>
2026-01-21 15:41:21 -0300

    fix: add repository annotation to repositories
    closes #47

--- src/main/java/com/voluntree/backend/repository/OrganizationRepository.java
+++ src/main/java/com/voluntree/backend/repository/OrganizationRepository.java
@@ -1,9 +1,11 @@
 package com.voluntree.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.voluntree.backend.domain.organization.Organization;
 
+@Repository
 public interface OrganizationRepository extends JpaRepository<Organization, Long> {
 
 }
--- src/main/java/com/voluntree/backend/repository/VolunteerRepository.java
+++ src/main/java/com/voluntree/backend/repository/VolunteerRepository.java
@@ -1,9 +1,11 @@
 package com.voluntree.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.voluntree.backend.domain.volunteer.Volunteer;
 
+@Repository
 public interface VolunteerRepository extends JpaRepository<Volunteer, Long> {
 
 }

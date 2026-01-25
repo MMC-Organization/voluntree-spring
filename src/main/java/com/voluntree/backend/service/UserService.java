9f5eb65da2fb82e259860d8b3be42cd07e274a0c
Arthur <arthurfelipe5567@gmail.com>
2026-01-25 17:04:56 -0300

    fix: fix user service to use log service

--- src/main/java/com/voluntree/backend/service/UserService.java
+++ src/main/java/com/voluntree/backend/service/UserService.java
@@ -1,6 +1,5 @@
 package com.voluntree.backend.service;
 
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,9 +14,7 @@ import com.voluntree.backend.dto.signup.VolunteerRequest;
 import com.voluntree.backend.dto.signup.VolunteerResponse;
 import com.voluntree.backend.enums.ActionType;
 import com.voluntree.backend.enums.Module;
-import com.voluntree.backend.enums.Outcome;
 import com.voluntree.backend.enums.UserType;
-import com.voluntree.backend.events.AuditEvent;
 import com.voluntree.backend.repository.OrganizationRepository;
 import com.voluntree.backend.repository.VolunteerRepository;
 
@@ -30,72 +27,68 @@ public class UserService {
   private final PasswordEncoder passwordEncoder;
   private final VolunteerRepository volunteerRepository;
   private final OrganizationRepository organizationRepository;
-  private final ApplicationEventPublisher eventPublisher;
+  private final LogService logService;
 
   @Transactional
   public VolunteerResponse registerVolunteer(VolunteerRequest data) {
-    Volunteer volunteer = new Volunteer();
+    try {
+      Volunteer volunteer = new Volunteer();
 
-    volunteer.setName(data.name());
-    volunteer.setEmail(data.email());
-    volunteer.setPassword(passwordEncoder.encode(data.password()));
-    volunteer.setPhoneNumber(data.phoneNumber());
-    volunteer.setCep(data.cep());
-    volunteer.setNumber(data.number());
-    volunteer.setCpf(new Cpf(data.cpf()));
+      volunteer.setName(data.name());
+      volunteer.setEmail(data.email());
+      volunteer.setPassword(passwordEncoder.encode(data.password()));
+      volunteer.setPhoneNumber(data.phoneNumber());
+      volunteer.setCep(data.cep());
+      volunteer.setNumber(data.number());
+      volunteer.setCpf(new Cpf(data.cpf()));
 
-    Volunteer savedVolunteer = volunteerRepository.save(volunteer);
+      Volunteer savedVolunteer = volunteerRepository.save(volunteer);
 
-    AuditEvent event = new AuditEvent(
-        "Novo voluntário cadastrado: " + savedVolunteer.getEmail(),
-        savedVolunteer.getId(),
-        savedVolunteer.getId(),
-        UserType.VOLUNTEER,
-        ActionType.CREATE,
-        Outcome.SUCCESS,
-        Module.AUTH);
+      logService.saveSuccessLog("Novo voluntário cadastrado!", null, savedVolunteer.getId(), UserType.VOLUNTEER,
+          ActionType.CREATE, Module.AUTH);
 
-    eventPublisher.publishEvent(event);
-
-    return new VolunteerResponse(
-        savedVolunteer.getName(),
-        savedVolunteer.getEmail());
+      return new VolunteerResponse(
+          savedVolunteer.getName(),
+          savedVolunteer.getEmail());
+    } catch (Exception e) {
+      logService.saveFailureLog("Erro criando voluntário de email " + data.email() + ": " + e.getMessage(), null, null,
+          UserType.VOLUNTEER, ActionType.CREATE, Module.AUTH);
+      throw e;
+    }
   }
 
   @Transactional
   public OrganizationResponse registerOrganization(OrganizationRequest data) {
-    Organization organization = new Organization();
+    try {
+      Organization organization = new Organization();
 
-    organization.setName(data.name());
-    organization.setEmail(data.email());
-    organization.setPassword(passwordEncoder.encode(data.password()));
-    organization.setPhoneNumber(data.phoneNumber());
-    organization.setCep(data.cep());
-    organization.setNumber(data.number());
+      organization.setName(data.name());
+      organization.setEmail(data.email());
+      organization.setPassword(passwordEncoder.encode(data.password()));
+      organization.setPhoneNumber(data.phoneNumber());
+      organization.setCep(data.cep());
+      organization.setNumber(data.number());
 
-    organization.setCnpj(new Cnpj(data.cnpj()));
-    organization.setCompanyName(data.companyName());
-    organization.setCause(data.cause());
+      organization.setCnpj(new Cnpj(data.cnpj()));
+      organization.setCompanyName(data.companyName());
+      organization.setCause(data.cause());
 
-    Organization savedOrganization = organizationRepository.save(organization);
+      Organization savedOrganization = organizationRepository.save(organization);
 
-    AuditEvent event = new AuditEvent(
-        "Nova organização cadastrada: " + savedOrganization.getCompanyName(),
-        savedOrganization.getId(),
-        savedOrganization.getId(),
-        UserType.ORGANIZATION,
-        ActionType.CREATE,
-        Outcome.SUCCESS,
-        Module.AUTH);
+      logService.saveSuccessLog("Nova organização cadastrada!", null, savedOrganization.getId(), UserType.ORGANIZATION,
+          ActionType.CREATE, Module.AUTH);
 
-    eventPublisher.publishEvent(event);
-
-    return new OrganizationResponse(
-        savedOrganization.getName(),
-        savedOrganization.getEmail(),
-        savedOrganization.getPhoneNumber(),
-        savedOrganization.getCep(),
-        savedOrganization.getCompanyName(),
-        savedOrganization.getCause());
+      return new OrganizationResponse(
+          savedOrganization.getName(),
+          savedOrganization.getEmail(),
+          savedOrganization.getPhoneNumber(),
+          savedOrganization.getCep(),
+          savedOrganization.getCompanyName(),
+          savedOrganization.getCause());
+    } catch (Exception e) {
+      logService.saveFailureLog("Erro criando organização de email " + data.email() + ": " + e.getMessage(), null, null,
+          UserType.ORGANIZATION, ActionType.CREATE, Module.AUTH);
+      throw e;
+    }
   }
 }

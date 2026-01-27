4484d36979110eff3210fa7bb1f892f585634c0e
Arthur <arthurfelipe5567@gmail.com>
2026-01-26 21:20:07 -0300

    refactor: remove log repo from logout handler
    replace it with the log service

--- src/main/java/com/voluntree/backend/config/security/CustomLogoutSuccessHandler.java
+++ src/main/java/com/voluntree/backend/config/security/CustomLogoutSuccessHandler.java
@@ -6,11 +6,9 @@ import org.springframework.security.web.authentication.logout.LogoutSuccessHandl
 import org.springframework.stereotype.Component;
 
 import com.voluntree.backend.domain.CustomUserDetails;
-import com.voluntree.backend.domain.Log;
 import com.voluntree.backend.enums.ActionType;
 import com.voluntree.backend.enums.Module;
-import com.voluntree.backend.enums.Outcome;
-import com.voluntree.backend.repository.LogRepository;
+import com.voluntree.backend.service.LogService;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -19,13 +17,14 @@ import lombok.RequiredArgsConstructor;
 @Component
 @RequiredArgsConstructor
 public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
-  private final LogRepository repo;
+
+  private final LogService logService;
 
   @Override
   public void onLogoutSuccess(HttpServletRequest req, HttpServletResponse res, @Nullable Authentication auth) {
     if (auth != null && auth.getPrincipal() instanceof CustomUserDetails user) {
-      repo.save(new Log("Usuário desconectado!", user.getUserId(), null, user.getUserType(),
-          ActionType.SIGNOUT, Outcome.SUCCESS, Module.AUTH));
+      logService.saveSuccessLog("Usuário desconectado!", user.getUserId(), null, user.getUserType(),
+          ActionType.SIGNOUT, Module.AUTH);
     }
 
     res.setStatus(HttpServletResponse.SC_NO_CONTENT);

ba673528558dd9a662386fc639b16ef11f0aa061
Arthur <arthurfelipe5567@gmail.com>
2026-01-26 21:57:00 -0300

    fix: add validation to dtos
    refs #56

--- src/main/java/com/voluntree/backend/dto/profile/UpdateProfileRequest.java
+++ src/main/java/com/voluntree/backend/dto/profile/UpdateProfileRequest.java
@@ -1,10 +1,30 @@
 package com.voluntree.backend.dto.profile;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
 public record UpdateProfileRequest(
+    
+    @Size(max = 150)
+    @NotEmpty(message = "O nome não pode ser vazio")
     String name,
+
+    @Size(max = 255)
+    @NotEmpty
+    @Email(message = "E-mail inválido")
     String email,
+    
+    @Size(max = 25)
+    @NotEmpty
     String phoneNumber,
+
+    @Size(min = 8, max = 8)
+    @NotEmpty
     String cep,
+
+    @Size(max = 10)
+    @NotEmpty
     String number
 ) {}
 

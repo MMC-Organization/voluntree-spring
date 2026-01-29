2ffafa797ef7d7b619249d44e0f33f58aca4e6b8
Arthur <arthurfelipe5567@gmail.com>
2026-01-29 09:43:12 -0300

    fix: remove user creation runner

--- src/main/java/com/voluntree/backend/BackendApplication.java
+++ src/main/java/com/voluntree/backend/BackendApplication.java
@@ -18,23 +18,4 @@ public class BackendApplication {
   public static void main(String[] args) {
     SpringApplication.run(BackendApplication.class, args);
   }
-
-  @Bean
-  CommandLineRunner initUserTest(UserRepository repo, PasswordEncoder encoder) {
-    return args -> {
-      if (!repo.existsByEmail("test@user.com")) {
-        Volunteer vol = new Volunteer();
-
-        vol.setName("arthur");
-        vol.setEmail("test@user.com");
-        vol.setPhoneNumber("998459868");
-        vol.setCpf(new Cpf("15024982436"));
-        vol.setCep("58052310");
-        vol.setPassword(encoder.encode("euamocomercoco"));
-
-        repo.save(vol);
-      }
-    };
-  }
-
 }
